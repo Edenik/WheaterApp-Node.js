a4a24e161ff99d0f33dc773246a7e7c5457027a9
Edenik <Edenik5@gmail.com>
2020-12-09 22:43:41 +0200

    Handling Errors

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const request = require('request');
 require('dotenv').config()
 
+
 const weatherURL = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=37.8267,-122.4233`
-const mapBoxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_API_KEY}&limit=1`
+request({ url: weatherURL, json: true }, (err, res) => {
+    if (err) {
+        console.log('Unable to connect to Weather service!')
+    } else if (res.body.error) {
+        console.log('Unable to find location!')
+    } else {
+        const tempData = res.body.current;
+        const locData = res.body.location;
+        console.log(`${tempData.weather_descriptions[0]} It is currently ${tempData.temperature} 
+            degrees in ${locData.name} of ${locData.country}. 
+            It feels like ${tempData.feelslike} degrees out.`)
 
-
-request({url:mapBoxURL,json:true}, (err,res) =>{
-    const data = res.body.features[0];
-    console.log(data)
-    console.log(data.place_name + ', Latitude: ' + data.center[0]  + ' Longitude: '+ data.center[1] );
+    }
 })
-// request({ url: weatherURL, json: true }, (err, res) => {
-//     const tempData = res.body.current;
-//     const locData = res.body.location;
-//     console.log(`${tempData.weather_descriptions[0]} It is currently ${tempData.temperature} 
-//         degrees in ${locData.name} of ${locData.country}. 
-//         It feels like ${tempData.feelslike} degrees out.`)
 
-// })
+
+const mapBoxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_API_KEY}&limit=1`
+request({ url: mapBoxURL, json: true }, (err, res) => {
+    if (err) {
+        console.log('Unable to connect to Geocoding service!')
+    } else if (res.body.features.length === 0) {
+        console.log('Unable to get Geocoding from location')
+    } else {
+        const data = res.body.features[0];
+        console.log(data.place_name + ', Latitude: ' + data.center[0] + ' Longitude: ' + data.center[1]);
+    }
+})
+
 
