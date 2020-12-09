c58a39ceea47249bbad6d076bad829bcb3912a75
Edenik <Edenik5@gmail.com>
2020-12-09 22:07:18 +0200

    Customizing HTTP request

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const request = require('request');
 require('dotenv').config()
 
 const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=37.8267,-122.4233`
+console.log(url)
+request({ url: url, json: true }, (err, res) => {
+    // console.log(res.body.current)
+    const tempData = res.body.current;
+    const locData = res.body.location;
+    console.log(`${tempData.weather_descriptions[0]} It is currently ${tempData.temperature} 
+        degrees in ${locData.name} of ${locData.country}. 
+        It feels like ${tempData.feelslike} degrees out.`)
 
-request({url:url}, (err, res) => {
-    const data = JSON.parse(res.body)
-    console.log(data.current)
 })
