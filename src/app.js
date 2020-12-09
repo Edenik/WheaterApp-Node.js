eff905027e283e26428ad2551ee5757b2bf2a1ce
Edenik <Edenik5@gmail.com>
2020-12-10 00:58:25 +0200

    Dynamic Pages with Templating

--- src/app.js
+++ src/app.js
@@ -4,8 +4,26 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine' , 'hbs')
+app.set('views', path.join(__dirname, '../views'))
 app.use(express.static(publicDirectoryPath))
 
+app.get('' , (req,res) => {
+    res.render('index', {
+        title : 'Weather App',
+        name: 'Eden Nahum'
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.render('about' , {
+        title:"About"
+    })
+})
+
+app.get('/help',(req,res) => {
+    res.render('help', { title:"Help", message:"How can I help You?"})
+})
 app.get('/weather', (req,res) => {
     res.send({forecast:'it is raining', location:'Israel'})
 })
