6bc3794ae39b4885e0a601d2a429203f4c12a40a
Edenik <Edenik5@gmail.com>
2020-12-10 00:10:54 +0200

    Serving up HTML and JSON

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res)=> {
-    res.send('Hello Express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req,res) => {
@@ -11,10 +11,10 @@ app.get('/help', (req,res) => {
 })
 
 app.get('/about' ,(req,res) => {
-    res.send('About page')
+    res.send('<h1>About</h1>')
 })
 
 app.get('/weather', (req,res) => {
-    res.send('Wheater')
+    res.send({forecast:'it is raining', location:'Israel'})
 })
 app.listen(3000, ()=> console.log('Server is running on port 3000'))
