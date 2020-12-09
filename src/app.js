dfc5f8d426acd4e7d5c29a3a47b019228b0fad0e
Edenik <Edenik5@gmail.com>
2020-12-10 00:20:48 +0200

    Serving up Static Assets

--- src/app.js
+++ src/app.js
@@ -1,20 +1,13 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res)=> {
-    res.send('<h1>Weather</h1>')
-})
-
-app.get('/help', (req,res) => {
-    res.send('Help page')
-})
-
-app.get('/about' ,(req,res) => {
-    res.send('<h1>About</h1>')
-})
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req,res) => {
     res.send({forecast:'it is raining', location:'Israel'})
 })
+
 app.listen(3000, ()=> console.log('Server is running on port 3000'))
